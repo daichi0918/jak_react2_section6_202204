80ef05d3e0d5ecdb4759490d72cfc157c5f57e00
daichi0918 <74330571+daichi0918@users.noreply.github.com>
2022-04-23 10:39:55 +0900

    section7完了

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from "react";
+import { UserProvider } from "./providers/UserProvider";
 import { Router } from "./router/Router";
+import { RecoilRoot } from "recoil";
+
 import "./styles.css";
 
 export default function App() {
-  return <Router />;
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
+  );
 }
